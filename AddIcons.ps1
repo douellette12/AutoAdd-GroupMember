e03998e54fafd5265a2203067b1176aed1672fab
douellette12 <39159681+douellette12@users.noreply.github.com>
2018-05-14 16:02:41 -0400

    Update AddIcons
    with name match regex

--- AddIcons.ps1
+++ AddIcons.ps1
@@ -66,16 +66,52 @@ elseif ($ui -eq $option[6])
 		break
 		}
 		$user = $user.ToLower()
-		$name = Get-ADUser -Server $server -Filter {DisplayName -like $user} -Properties *
-		$member = Get-ADUser -Server $server -Identity $user -Properties *
+		if ($user -match ".\w\s.\w") #FirstName LastName
+		{
+			$member = Get-ADUser -Server $server -Filter {DisplayName -like $user} -Properties *
+		}
+		elseif ($user -match ".\w,\s.\w") #LastName, FirstName
+		{
+			$member = Get-ADUser -Server $server -Filter {DisplayName -like $user} -Properties *
+		}
+		elseif ($user -match ".\w\s[a-zA-Z]{1}\..\w") #First M. Last
+		{
+			$member = Get-ADUser -Server $server -Filter {DisplayName -like $user} -Properties *
+		}
+		elseif ($user -match "[a-zA-Z]{1}\d") #Wheaton IDs one letter and 6 numbers
+		{
+			$member = Get-ADUser -Server $server -Identity $user -Properties *
+		}
+		elseif ($user -match "[a-zA-z]{4}[0-9]{4}") #4x4
+		{
+			$member = Get-ADUser -Server $server -Identity $user -Properties *
+		}
+		elseif  ($user -match "[a-zA-z]{7}") #ShortName
+		{
+			$member = Get-ADUser -Server $server -Identity $user -Properties *
+		}
 		if ($member -ne $null)
 		{
 		$member | select DisplayName, Title, Department, Division
-		
+		#ForEach ($group in $groups) # Loop Through each group and find the members
+		#		{
+		#			$ismember = Get-ADGroup $group -Properties Member | Select-Object -ExpandProperty Member | Get-ADUser | Select SamAccountname
+		#			If ($ismember -contains $member)
+		#			{
+		#				Write-Host "$user is already a member of $group"
+		#				
+		#			}
+		#			else 
+		#			{
+		#				Write-Host "$user is not a member of $group"
+		#			}
+		#		}
+		#				
 		$ru = (Read-Host -Prompt "Would you like to proceed?(Y/N)")
 		$ru = $ru.ToLower()
-		if ($ru -eq "y"
+		if ($ru -like 'y*')
 		{
+				
 			ForEach ($group in $groups)
 				{
 				Write-Host "Adding $group group"
@@ -83,32 +119,12 @@ elseif ($ui -eq $option[6])
 				}
 				Write-Host ("Groups Added Successfully! Hit enter to quit, or enter another username to add to $ui groups.")
 		}
-		else
+		else 
 		{
 			exit
 		}
 		}
-		if ($name -ne $null)
-		{
-		$name | select DisplayName, Title, Department, Division
-		
-		$ru = (Read-Host -Prompt "Would you like to proceed?(Y/N)"
-		$ru = $ru.ToLower()
-		if ($ru -eq "y")
-		{
-			ForEach ($group in $groups)
-				{
-				Write-Host "Adding $group group"
-		    	Add-ADGroupMember -Identity $group -Members $name
-				}
-				Write-Host ("Groups Added Successfully! Hit enter to quit, or enter another FullName to add to $ui groups.")
-		}
-		else
-		{
-			exit
-		}
-		}
-	if ($name -eq $null -and $member -eq $null)
+	if ($member -eq $null)
 		{
 			Write-Host "The ADusername or Fullname specified was not found...Try again."
 			continue
